91f03ae9a58bb7270f0a57097e6c31b68c2880e8
Shurricane <sakol.sarakorn@contentfirstcambodia.com>
2024-03-29 13:27:19 +0700

    refactor: modify  spec to properly test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import Countries from '../../views/Countries.vue'
 
-describe('HelloWorld', () => {
+describe('Countries', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(Countries)
+    expect(wrapper.text()).toContain('Countries')
   })
 })
